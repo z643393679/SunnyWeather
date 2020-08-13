b5fbadb18aa0a5edb9477c975b44345fa94224a4
643393679 <643393679@qq.com>
2020-08-13 19:09:44 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.example.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application(){
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+
+        const val TOKEN="填入申请的令牌值"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context=applicationContext
+    }
+}
